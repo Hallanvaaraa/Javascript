706c2ea3c10891e75c543b4e4ace23f26c5a2e7e
Hallanvaaraa <tommi.s.halla@gmail.com>
2023-11-15 13:34:01 +0200

    15.11

--- Moduuli2/Tehtävä4/t4.js
+++ Moduuli2/Tehtävä4/t4.js
@@ -5,6 +5,6 @@ while (num != 0) {
   num_list.push(num);
   num = +prompt('Give a number or enter 0 to end:');
 }
-num_list.sort();
+num_list.sort((a, b) => a - b);
 num_list.reverse();
 console.log(num_list);
